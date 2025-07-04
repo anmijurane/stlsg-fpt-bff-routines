f42ecbfb03aaa243779fd5c282aff5347827e599
M. Andres Jurado <anmijurane@gmail.com>
2025-07-04 08:06:12 -0600

    feat: initialize main application entry point with NestJS setup

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const PORT = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(PORT);
+  new Logger('BFF-ROUTINES').log(`[UP] Listening app in port ${PORT}`);
+}
+bootstrap();
