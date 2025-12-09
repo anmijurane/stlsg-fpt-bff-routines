64ec7ba1740179d2f26e000fafb7fac1cc0e7cb5
Andres Jurado anmijurane <anmijurane@gmail.com>
2025-12-09 14:24:06 -0600

    FEAT: interactions module add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import {} from '@nestjs/core';
 import { FeedbackModule } from './feedback/feedback.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostgresTypeORMModule } from './modules.exports';
+import { InteractionsModule } from './interactions/interactions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PostgresTypeORMModule(), FeedbackModule],
+  imports: [ConfigModule.forRoot(), PostgresTypeORMModule(), FeedbackModule, InteractionsModule],
   controllers: [],
   providers: [],
 })
