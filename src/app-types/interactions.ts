2b45af3ed0e54d2a62840e6b61ef4c57bdf8a3bf
Andres Jurado anmijurane <anmijurane@gmail.com>
2025-12-22 00:21:14 -0600

    FEAT: interations types

--- src/app-types/interactions.ts
+++ src/app-types/interactions.ts
@@ -13,3 +13,30 @@ export interface PaginationI {
   page: number;
   limit: number;
 }
+
+export type RoutineType = 'adaptation' | 'muscle-gain' | 'health' | 'fat-burning';
+
+export type EventType = 'home' | 'attention_view' | 'exercise_view' | 'routine_view' | 'feedback' | 'other';
+
+export type TypeView = 'home' | 'exercise' | 'routine' | 'attention';
+
+export interface PageViewBuilder {
+  routine: RoutineType;
+  session_id: string;
+  page_path: string;
+  query_string: string;
+  level_id: number;
+  visited_at: Date;
+}
+
+export interface EventBuilder {
+  session_id: string;
+  type: EventType;
+  exercise_id: string;
+  exercise_name: string;
+  routine: RoutineType;
+  level_id: number;
+  day_routine: number;
+}
+
+export type BuilderEvent = Record<TypeView, () => Promise<{ page_view: PageViewBuilder; event: EventBuilder }>>;
