2b2db457de9a66e9b436ac883eb329174e25ed13
Andres Jurado anmijurane <anmijurane@gmail.com>
2025-12-22 00:31:52 -0600

    FEAT: registry module health

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { FeedbackModule } from './feedback/feedback.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostgresTypeORMModule } from './modules.exports';
 import { InteractionsModule } from './interactions/interactions.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
+    HealthModule,
     ConfigModule.forRoot(),
     PostgresTypeORMModule(),
     FeedbackModule,
