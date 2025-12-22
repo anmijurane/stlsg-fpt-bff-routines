8fcf102bee3d4e2ae3b319b7f8be466e4799d51c
Andres Jurado anmijurane <anmijurane@gmail.com>
2025-12-22 00:38:48 -0600

    feat: Add interaction creation endpoint, introduce new database schema scripts, and refine feedback service logic.

--- src/interactions/interactions.controller.ts
+++ src/interactions/interactions.controller.ts
@@ -4,13 +4,16 @@ import { CreateInteractionDto } from './dto/create-interaction.dto';
 import { GetInteractionsDto } from './dto/get-interactions.dto';
 import { GetEmojiTotalDto } from './dto/get-emoji-total.dto';
 import { GetCommentsDto } from './dto/get-comments.dto';
+import { DateTime } from 'luxon';
 
 @Controller('/v1/analytics')
 export class InteractionsController {
   constructor(private readonly interactionsService: InteractionsService) {}
 
   @Post('/create/interaction')
-  createInteraction() {}
+  createInteraction(@Body() body: CreateInteractionDto) {
+    return this.interactionsService.create(body);
+  }
 
   @Post('/interactions')
   create(@Body() body: GetInteractionsDto) {}
