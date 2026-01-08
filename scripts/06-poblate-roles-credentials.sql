99a1570e98e19acf1bd43ba78e1e227f2bf66e25
M. Andres Jurado <anmijurane@gmail.com>
2026-01-08 13:24:01 -0600

    feat: root admin generic access_key

--- scripts/06-poblate-roles-credentials.sql
+++ scripts/06-poblate-roles-credentials.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 INSERT INTO roles (name, description) VALUES
 ('root', 'Clearly the god the database'),
 ('admin', 'Allows you to create and query values, but cannot delete anything.'),
@@ -17,35 +19,13 @@ INSERT INTO credentials (
 )
 VALUES
 (
-  'developer',
-  'stlsg_developer_anmi',
+  'root',
+  'stlsg_routines_root',
   'sk_',
-  'eadc3b185a0920b8d211048b646b687d7f1005df',
+  encode(digest(gen_random_bytes(32), 'sha1'), 'hex'),
   TRUE,
   NOW(),
   NOW() + INTERVAL '2 years',
   2,
   'Acceso completo para administración del sistema'
-),
-(
-  'fpt-routines-site-ssr',
-  'fpt-routines-site',
-  '',
-  '',
-  TRUE,
-  NOW(),
-  NOW() + INTERVAL '2 years',
-  3,
-  'Solo permite crear registros, no consultar'
-),
-(
-  'fpt-power-bi',
-  'fpt-power-bi',
-  '',
-  '',
-  TRUE,
-  NOW(),
-  NOW() + INTERVAL '1 year',
-  4,
-  'Acceso únicamente de lectura'
 );
